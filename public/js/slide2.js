5a6dcb67d21ed6dbd269137aa8dbc3ebe99f7003
booldook <booldook@gmail.com>
2019-10-18 13:16:54 +0900

    slide2 완성

--- public/js/slide2.js
+++ public/js/slide2.js
@@ -2,22 +2,24 @@ var now = 0;
 $(".bt-prev").click(function(){
 	if(now > 0) now--;
 	ani();
-});
+}).hide();
 $(".bt-next").click(function(){
 	if(now < 4) now++;
 	ani();
 });
 function ani() {
-	$(".banners").stop().animate({"left": (-720*now)+"px"}, 500, function(){
-		if(now == 0) {
-
-		}
-		else if(now == 4) {
-
-		}
-		else {
-			
-		}
-	});
+	$(".banners").stop().animate({"left": (-720*now)+"px"}, 500);
+	if(now == 0) {
+		$(".bt-prev").hide();
+		$(".bt-next").show();
+	}
+	else if(now == 4) {
+		$(".bt-prev").show();
+		$(".bt-next").hide();
+	}
+	else {
+		$(".bt-prev").show();
+		$(".bt-next").show();
+	}
 }
 
