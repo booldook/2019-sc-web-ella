66578a484dfd4f283f3c3cca13d786e2c99bc578
booldook <booldook@gmail.com>
2019-10-22 11:39:00 +0900

    11, 12 번 카피 완료

---
+++ public/js/slide12.js
@@ -0,0 +1,90 @@
+/* 변수 타입 
+primitive type: 
+	Number(0 == false), 
+	String("" == false), 
+	Boolean, 
+	undefined == false, 
+	null == false
+refernce type: Array, Object
+const a = 5;
+a = 10; // Error
+const b = [];
+b[1] = 10; // No Error
+b = [1, 2, 3]; // Error
+*/
+
+// 전역변수
+var now = 0;
+var interval;
+
+// 이벤트
+$(".bt-prev").click(function(){
+	if(now > 0) now--;
+	init();
+}).hide();
+$(".bt-next").click(function(){
+	if(now < 4) now++;
+	init();
+});
+$(".pager").click(function(){
+	now = $(this).index();
+	init();
+});
+$(".banners-wrap").mouseover(function(){
+	clearInterval(interval);
+});
+$(".banners-wrap").mouseleave(function(){
+	clearInterval(interval);
+	interval = setInterval(intervalCb, 2000);
+});
+// Interval CallBack
+function intervalCb() {
+	now++;
+	init();
+}
+
+/* 동작 */
+// 시작할 때 한번 실행
+(function () {
+	pagerInit();
+	interval = setInterval(intervalCb, 2000);
+})();
+// 이벤트 발생할 때 실행할 함수
+function init() {
+	ani();
+	btInit();
+	pagerInit();
+}
+// 애니메이션
+function ani() {
+	$(".banners").stop().animate({"left": (-720*now)+"px"}, 500, function(){
+		if(now == 5) {
+			now = 0;
+			$(".banners").css("left", 0);
+			pagerInit();
+			btInit();
+		}
+	});
+}
+// 버튼 정렬
+function btInit() {
+	if(now == 0) {
+		$(".bt-prev").hide();
+		$(".bt-next").show();
+	}
+	else if(now == 4) {
+		$(".bt-prev").show();
+		$(".bt-next").hide();
+	}
+	else {
+		$(".bt-prev").show();
+		$(".bt-next").show();
+	}
+}
+// 페이저 정렬
+function pagerInit() {
+	$(".pager").removeClass("active");
+	$(".pager").eq(now).addClass("active");
+}
+
+
