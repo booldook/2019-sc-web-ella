02ccb46b8e5f43c095b6e66dfaeb46eeb6a808ae
booldook <booldook@gmail.com>
2019-10-22 10:18:20 +0900

    08.slide

--- public/js/slide8.js
+++ public/js/slide8.js
@@ -1,8 +1,25 @@
 /* 전역변수 */
-
+var now = 4;			// 페이드 될 LI
+var depth = 10;		// 계속 증가될 z-index
+var speed = 1000;	// opacity 속도
+var delay = 3000;	// Animation Delay
+var interval;
 
 /* 이벤트 */
-
+interval = setInterval(intervalCb, delay);
+function intervalCb() {
+	/*
+	if(now == 4) now = 0;
+	else now++;
+	if(depth == 800) {
+		$(".banner").css({"z-index": 9});
+		dpeth = 9;
+	}
+	*/
+	now == 4 ? now = 0 : now++;
+	$(".banner").eq(now).css({"z-index": depth++, "opacity": 0});
+	$(".banner").eq(now).stop().animate({"opacity": 1}, speed);
+}
 
 /* 동작 */
 
