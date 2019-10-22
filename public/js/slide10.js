4676b7623bf0ede34865e44b86bbd0ce250173c0
booldook <booldook@gmail.com>
2019-10-22 11:14:55 +0900

    10.slide-페이드-페이저-자동실행 완성

--- public/js/slide10.js
+++ public/js/slide10.js
@@ -1,5 +1,5 @@
 /* 전역변수 */
-var now = 4;			// 페이드 될 LI
+var now = 0;			// 페이드 될 LI
 var depth = 10;		// 계속 증가될 z-index
 var speed = 1000;	// opacity 속도
 var delay = 3000;	// Animation Delay
@@ -9,10 +9,24 @@ var interval;
 interval = setInterval(intervalCb, delay);
 function intervalCb() {
 	now == 4 ? now = 0 : now++;
-	$(".banner").eq(now).css({"z-index": depth++, "opacity": 0});
-	$(".banner").eq(now).stop().animate({"opacity": 1}, speed);
+	$(".pager").eq(now).trigger("click");
 }
+$(".pager").click(function(){
+	now = $(this).index();
+	$(".banner").eq(now).css({"z-index": depth++, "opacity": 0});
+	$(".banner").eq(now).stop().animate({"opacity": 1}, 1000);
+	$(".pager").removeClass("active");
+	$(this).addClass("active");
+});
+$(".pager").eq(now).trigger("click");
 
+$(".banners").mouseover(function(){
+	clearInterval(interval);
+});
+$(".banners").mouseleave(function(){
+	clearInterval(interval);
+	interval = setInterval(intervalCb, delay);
+});
 /* 동작 */
 
 
