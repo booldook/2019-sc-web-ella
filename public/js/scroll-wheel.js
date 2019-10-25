57d2620785fb048aafd008f069f7d48b0a9ab3e3
booldook <booldook@gmail.com>
2019-10-25 16:44:42 +0900

    booldook

--- public/js/scroll-wheel.js
+++ public/js/scroll-wheel.js
@@ -7,9 +7,12 @@ function wheelFn(){
 	var posY = event.pageY;
 	var n = Math.floor(posY/hei);
 	var pageCnt = $(".page").length;	//4
-	if(delta > 0) (n < pageCnt - 1) ? wheelAni((n + 1) * hei): "";	// 아래로 휠
-	else (n > 0) ? wheelAni((n - 1) * hei) : "";	// 위로 휠
+	if(delta > 0) (n < pageCnt - 1) ? wheelAni((n + 1) * hei, n, (n + 1)) : 0;	// 아래로 휠
+	else (n > 0) ? wheelAni((n - 1) * hei, n, (n - 1)) : 0;	// 위로 휠
 }
-function wheelAni(pos) {
-	$("html, body").stop().animate({"scrollTop": pos}, 1000);
+function wheelAni(pos, prevPage, nowPage) {
+	console.log(prevPage, nowPage);
+	$("html, body").stop().animate({"scrollTop": pos}, 1000, function(){
+
+	});
 }
