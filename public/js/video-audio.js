32c79d8828b45bfea8debaeeeb64f28970eac1fd
booldook <booldook@gmail.com>
2019-10-28 11:53:50 +0900

    19.video-youtube.html

--- public/js/video-audio.js
+++ public/js/video-audio.js
@@ -1,13 +1,12 @@
-$(".videos").vegas({
-	slides: [
-		{
-			video: {
-						src: [
-								'../video/sea-movie.mp4'
-						],
-						loop: true,
-						mute: true
-				}
-		}
-	]
-});
+$(window).resize(function(){
+	$(".videos").remove();
+	$("body").append('<div class="videos"></div>');
+	$(".videos").vegas({
+		slides: [{video: {src: ['../video/sea-movie.mp4'], loop: true, mute: true}}]
+	});
+}).trigger("resize");
+
+$.vegas.isVideoCompatible = function () {
+	var devices = /(Android|webOS|Phone|iPad|iPod|BlackBerry|Windows Phone)/i;
+	return !devices.test(navigator.userAgent);
+}
