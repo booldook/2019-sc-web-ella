a2bc750d73f49ff5dfb4b15447cfb221a57865c2
booldook <booldook@gmail.com>
2019-10-28 10:55:10 +0900

    vegas 적용

---
+++ public/js/video-audio.js
@@ -0,0 +1,14 @@
+$elmt = $(".videos")
+$(".videos").vegas({
+	slides: [
+		{
+			video: {
+						src: [
+								'../video/sea-movie.mp4'
+						],
+						loop: true,
+						mute: true
+				}
+		}
+	]
+});
