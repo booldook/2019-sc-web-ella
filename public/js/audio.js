00dc0ab81078c593bc21746c1da089b0be498cce
booldook <booldook@gmail.com>
2019-10-29 11:16:50 +0900

    audio

--- public/js/audio.js
+++ public/js/audio.js
@@ -1,25 +1,37 @@
 $("#bt-play").click(function(){
 	$(this).hide();
 	$("#bt-pause").show();
+	$("#snd")[0].play();
 });
 $("#bt-pause").click(function(){
 	$(this).hide();
 	$("#bt-play").show();
+	$("#snd")[0].pause();
 });
 $("#bt-stop").click(function(){
-
+	$("#snd")[0].pause();
+	$("#snd")[0].currentTime = 0;
+	$("#bt-pause").hide();
+	$("#bt-play").show();
 });
 $("#bt-muted").click(function(){
 	$(this).hide();
 	$("#bt-up").show();
+	$("#snd")[0].muted = true;
 });
 $("#bt-up").click(function(){
 	$(this).hide();
 	$("#bt-muted").show();
+	$("#snd")[0].muted = false;
 });
-$("#volumn").change(function(){
-	console.log($(this).val());
+$("#volume").change(function(){
+	$("#snd")[0].muted = false;
+	$("#snd")[0].volume = $(this).val();
+	$("#bt-muted").show();
+	$("#bt-up").hide();
 });
 $("#snd-sel").change(function(){
-	console.log($(this).val());
+	var src = $(this).val();
+	$("#snd")[0].src = src;
+	$("#bt-play").trigger("click");
 });
