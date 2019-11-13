b9c6833ab87c03b1cae7225a16d934d1c51896a0
booldook <booldook@gmail.com>
2019-11-13 11:55:12 +0900

    AdminLogin

--- model/AdminLogin.js
+++ model/AdminLogin.js
@@ -13,14 +13,8 @@ AdminLogin.init({
 	// timestamps: false
 });
 // force: true -> 기존에 테이블이 존재하면 삭제하고 다시만든다. 절대 쓰지말자
-(async () => {
-	const result = await AdminLogin.sync({force: true});
-	AdminLogin.create({
-		adminID: "booldook",
-		adminPW: "1234",
-		grade: 9
-	});
-});
+// sync() 메서드를 최초 한번 실행하여 테이블이 생성되면 주석처리 하여 더이상 쓸 필요가 없다.
+// AdminLogin.sync({force: false});
 
 
 
