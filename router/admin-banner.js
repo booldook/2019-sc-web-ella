b3b012c426aac0f08b71a29f23eb20eefa910ebb
booldook <booldook@gmail.com>
2019-11-27 10:07:15 +0900

    multer

--- router/admin-banner.js
+++ router/admin-banner.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const router = express.Router();
+const mt = require("../modules/multer-conn");
 const path = require("path");
 const { AdminBanner } = require(path.join(__dirname, "../model/AdminBanner"));
 const util = require(path.join(__dirname, "../modules/util"));
 
 /* REST */
 router.get("/:type", getData);
+router.post("/:type", mt.upload.single("src"), postData);
 
 /* Router CB */
 async function getData(req, res, next) {
@@ -28,5 +30,29 @@ async function getData(req, res, next) {
 			break;
 	}
 }
+
+async function postData(req, res, next) {
+	let type = req.params.type;
+	let title = req.body.title;
+	let position = req.body.position;
+	let link = req.body.link;
+	let desc = req.body.desc;
+	let src = "";
+	if(req.file) src = req.file.filename;
+
+	switch(type) {
+		case "top":
+			
+			break;
+		case "bottom":
+			break;
+		default:
+			next();
+			break;
+	}
+}
+
+
+
 
 module.exports = router;
